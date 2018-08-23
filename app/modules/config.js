89cf864d11745895f930dca621b8381e0df09b51
easonzen <376575887@qq.com>
2018-08-23 21:08:54 +0800

    修改图片路径

--- app/modules/config.js
+++ app/modules/config.js
@@ -1,165 +1,165 @@
 const keywords = [
     {
-        img: require('../../static/images/keywords/biaogan.png'),
+        img: require('../../static/images/biaogan.png'),
         label: '标杆'
     },
     {
-        img: require('../../static/images/keywords/buwangchuxin.png'),
+        img: require('../../static/images/buwangchuxin.png'),
         label: '不忘初心'
     },
     {
-        img: require('../../static/images/keywords/chaoyue.png'),
+        img: require('../../static/images/chaoyue.png'),
         label: '超越'
     },
     {
-        img: require('../../static/images/keywords/chuxin.png'),
+        img: require('../../static/images/chuxin.png'),
         label: '初心'
     },
     {
-        img: require('../../static/images/keywords/jianerbufan.png'),
+        img: require('../../static/images/jianerbufan.png'),
         label: '简而不凡'
     },
     {
-        img: require('../../static/images/keywords/jiangxin.png'),
+        img: require('../../static/images/jiangxin.png'),
         label: '匠心'
     },
     {
-        img: require('../../static/images/keywords/jiangxinduju.png'),
+        img: require('../../static/images/jiangxinduju.png'),
         label: '匠心独具'
     },
     {
-        img: require('../../static/images/keywords/jiaotashidi.png'),
+        img: require('../../static/images/jiaotashidi.png'),
         label: '脚踏实地'
     },
     {
-        img: require('../../static/images/keywords/jizhi.png'),
+        img: require('../../static/images/jizhi.png'),
         label: '极致'
     },
     {
-        img: require('../../static/images/keywords/naizhujimo.png'),
+        img: require('../../static/images/naizhujimo.png'),
         label: '耐住寂寞'
     },
     {
-        img: require('../../static/images/keywords/nianqing.png'),
+        img: require('../../static/images/nianqing.png'),
         label: '年轻'
     },
     {
-        img: require('../../static/images/keywords/qidian.png'),
+        img: require('../../static/images/qidian.png'),
         label: '起点'
     },
     {
-        img: require('../../static/images/keywords/rencheshenghuo.png'),
+        img: require('../../static/images/rencheshenghuo.png'),
         label: '人车生活'
     },
     {
-        img: require('../../static/images/keywords/rexuefeiteng.png'),
+        img: require('../../static/images/rexuefeiteng.png'),
         label: '热血沸腾'
     },
     {
-        img: require('../../static/images/keywords/shuanghequdong.png'),
+        img: require('../../static/images/shuanghequdong.png'),
         label: '双核驱动'
     },
     {
-        img: require('../../static/images/keywords/taidu.png'),
+        img: require('../../static/images/taidu.png'),
         label: '态度'
     },
     {
-        img: require('../../static/images/keywords/weilaikeqi.png'),
+        img: require('../../static/images/weilaikeqi.png'),
         label: '未来可期'
     },
     {
-        img: require('../../static/images/keywords/woaigongzuo.png'),
+        img: require('../../static/images/woaigongzuo.png'),
         label: '我爱工作'
     },
     {
-        img: require('../../static/images/keywords/xingchendahai.png'),
+        img: require('../../static/images/xingchendahai.png'),
         label: '星辰大海'
     },
     {
-        img: require('../../static/images/keywords/xingdonggangling.png'),
+        img: require('../../static/images/xingdonggangling.png'),
         label: '行动纲领'
     },
     {
-        img: require('../../static/images/keywords/xinnian.png'),
+        img: require('../../static/images/xinnian.png'),
         label: '信念'
     },
     {
-        img: require('../../static/images/keywords/yidinghuihuo.png'),
+        img: require('../../static/images/yidinghuihuo.png'),
         label: '一定会火'
     },
     {
-        img: require('../../static/images/keywords/yongwuzhijing.png'),
+        img: require('../../static/images/yongwuzhijing.png'),
         label: '永无止境'
     },
     {
-        img: require('../../static/images/keywords/zhuanzhu.png'),
+        img: require('../../static/images/zhuanzhu.png'),
         label: '专注'
     }
 ];
 
 const cars = [
     {
-        img: require('../../static/images/cars/2003sunny.png'),
+        img: require('../../static/images/2003sunny.png'),
         label: '2003 SUNNY阳光'
     },
     {
-        img: require('../../static/images/cars/2004bluebird.png'),
+        img: require('../../static/images/2004bluebird.png'),
         label: '2004 BLUEBIRD蓝鸟'
     },
     {
-        img: require('../../static/images/cars/2005tida.png'),
+        img: require('../../static/images/2005tida.png'),
         label: '2005 TIIDA骐达'
     },
     {
-        img: require('../../static/images/cars/2006geniss.png'),
+        img: require('../../static/images/2006geniss.png'),
         label: '2006 GENISS骏逸'
     },
     {
-        img: require('../../static/images/cars/2007livina.png'),
+        img: require('../../static/images/2007livina.png'),
         label: '2007 LIVINA骊威'
     },
     {
-        img: require('../../static/images/cars/2008qashqai.png'),
+        img: require('../../static/images/2008qashqai.png'),
         label: '2008 QASHQAI逍客'
     },
     {
-        img: require('../../static/images/cars/2009xtrail.png'),
+        img: require('../../static/images/2009xtrail.png'),
         label: '2009 X-TRAIL奇骏'
     },
     {
-        img: require('../../static/images/cars/2010sunny.png'),
+        img: require('../../static/images/2010sunny.png'),
         label: '2010 SUNNY新阳光'
     },
     {
-        img: require('../../static/images/cars/2011murano.png'),
+        img: require('../../static/images/2011murano.png'),
         label: '2011 MURANO楼兰'
     },
     {
-        img: require('../../static/images/cars/2012xinxuanyi.png'),
+        img: require('../../static/images/2012xinxuanyi.png'),
         label: '2012 新轩逸'
     },
     {
-        img: require('../../static/images/cars/2013xinshidaitianlai.png'),
+        img: require('../../static/images/2013xinshidaitianlai.png'),
         label: '2013 新世代天籁'
     },
     {
-        img: require('../../static/images/cars/2014xinshidaiqijun.png'),
+        img: require('../../static/images/2014xinshidaiqijun.png'),
         label: '2014 新势代奇骏'
     },
     {
-        img: require('../../static/images/cars/2015quanxinxiaoke.png'),
+        img: require('../../static/images/2015quanxinxiaoke.png'),
         label: '2015 全新逍客'
     },
     {
-        img: require('../../static/images/cars/2016xima.png'),
+        img: require('../../static/images/2016xima.png'),
         label: '2016 西玛'
     },
     {
-        img: require('../../static/images/cars/2017jinke.png'),
+        img: require('../../static/images/2017jinke.png'),
         label: '2017 劲客'
     },
     {
-        img: require('../../static/images/cars/2018xuanyichundian.png'),
+        img: require('../../static/images/2018xuanyichundian.png'),
         label: '2018 轩逸・纯电'
     }
 ];
