e401724244a3e491c81e784c9aab1de9a893af24
easonzen <376575887@qq.com>
2018-08-23 11:06:56 +0800

    init project

---
+++ app/modules/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="first-app">
+                <h3 className="title">tiger-new</h3>
+                <p>To get started, edit <code>app/index.js</code> and save to reload.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
